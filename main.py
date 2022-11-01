043680b4d8104138a9aaca5225b22d766b36ff04
Aviral Dhingra <greatestpythoncoder287@gmail.com>
2022-11-01 18:35:46 +0530

    Commit...

--- main.py
+++ main.py
@@ -1,23 +1,26 @@
-MAX = 1500
-numbers = []
-numbers_2 = []
-perfect_squares = []
-
-x = int(MAX**(1/2))
-for i in range(1, x+1):
-    numbers.append(i)
+import numpy as np
 
 
-for num in numbers:
-    num = (num**2)
-    numbers_2.append(num)
+def itr(MAX):
+    l = []
+    x = int(MAX**(1/2))
+    for i in range(1, x+1):
+        l.append(i)
+    perfect_squares = []
+    for num in l:
+        num = (num**2)
+        perfect_squares.append(num)
 
-print(numbers_2)
-
-# for i in range(1,11):
-#     y = numbers_2.findall(i)
-#     print(y)
+    ending_digits = np.array(perfect_squares) % 10
+    ending_digits[ending_digits == 0] = 10
+    return perfect_squares, ending_digits
 
 
-
-    
+if __name__ == '__main__':
+    # Plotted Graphs : 500, 1500
+    MAX = int(input('Max : '))
+    perfect_squares, ending_digits = itr(MAX)
+    print(f'Perfect Squares : {perfect_squares}')
+    for i in range(1, 10+1):
+        frequency = np.count_nonzero(ending_digits == i)
+        print(str(i), '=>', str(frequency))
